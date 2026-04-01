dda84c1773c4469f7def3ea36a567e79f3e4959f
connectagent <connectagent@users.noreply.github.com>
2026-04-01 17:50:50 -0500

    publish: update source files

--- src/overrides.py
+++ src/overrides.py
@@ -1,7 +1,7 @@
 """URL overrides - modified by scripts/prepare_local_browser.py during build."""
 
 ### OVERRIDES START
-AUTHORIZE_DOMAIN = 'bootstrap_tunnel.connectagent.dev'
+AUTHORIZE_DOMAIN = 'bootstrap-tunnel.connectagent.dev'
 RELAY_URL=    "http://ufrelay.connectagent.dev/tunnel"
 #RELAY_URL=    "http://127.0.0.1:5051/tunnel" #local
 ### OVERRIDES END
